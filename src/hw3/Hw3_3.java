d883971cf86e68f6256f8539c45663cb4c5d0224
Jamie Wu <meigo1229@gmail.com>
2025-03-20 22:29:13 +0800

    first commit

--- src/hw3/Hw3_3.java
+++ src/hw3/Hw3_3.java
@@ -2,6 +2,25 @@
 //對則顯示正確訊息，如圖示結果：
 package hw3;
 
-public class Hw3_3 {
+import java.util.Scanner;
 
+public class Hw3_3 {
+	public static void main(String[] args) {
+		    
+		    Scanner sc = new Scanner(System.in);
+		    
+		    System.out.println("開始猜數字吧!(0~9)");
+		    int i = (int)(Math.random()*10);
+		   
+		    	
+		    while(i < 9) { 
+		    	int keyIn = sc.nextInt();
+		    	if(keyIn == i) {
+		    		System.out.println("答對了，答案就是" + keyIn);
+		    		break;
+		    	}
+		    	else {System.out.println("猜錯囉!");}
+		    	continue;
+		    }
+	}
 }
